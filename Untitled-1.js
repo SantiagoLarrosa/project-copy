4315fd61f521e1bbdb4011fa5d22d3794866ca82
Santiago Larrosa <santilarrosa30@gmail.com>
2017-10-27 09:57:37 -0300

    Initial commit

---
+++ Untitled-1.js
@@ -0,0 +1,29 @@
+var numero1=5;
+var numero2=8;
+
+if(!numero1>numero2){
+console.log("numero1 no es mayor que numero2");
+}
+if(numero2>0){
+    console.log("numero2 es positivo");
+
+}
+if(numero1<=0){
+    console.log("numero1 es negativo o distinto de cero");
+}
+if(!numero1+1>=numero2){
+    "Incrementar en 1 unidad el calor de numero1 no lo hace mayor o igual que numero2"
+}
+var array=[0,1,2,3];
+
+var contador = 0;
+while(contador<=array.length){
+    var numero= array[contador];
+    console.log("Numero:"+numero);
+contador++
+}
+
+var z=parseInt(prompt("numero entero"));
+for(i=z,i==0,i-){
+    i x (i-1)
+}
---
+++ script.js
@@ -0,0 +1,38 @@
+
+var usuario1={
+    user: "123456789",
+    password: "123456789",
+
+}
+var usuario2={
+    user: "987654321",
+    password: "987654321",
+}
+
+
+
+function login() {
+    var user = parseInt(document.getElementById("user").value);
+    var password = parseInt(document.getElementById("pasword").value);
+    if ((user == usuario1.user && password == usuario1.password) || (user == usuario2.user && password == usuario2.password)){
+        window.location="../internas/medicos.html";
+    }
+else{
+        alert("Ingrese un usuario y contraseña correctos")
+    }
+    
+    
+}
+function sendMail() {
+    var link = "santilarrosa30@gmail.com"
+      + "?cc=ConsultaRadio"
+      + "&subject=" + escape("Info Web")
+      + "&body=" + escape("Mi nombre es: " + document.getElementById('name').value);
+
+    var n = document.forms[0].name.value;
+    var m = document.forms[0].mail.value;
+    var t = document.getElementById("test");
+    var filter = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,8})+$/;
+    if (n.length >= 2 && filter.test(m)) { window.location.href = "mailto:" + link; return true; }
+    else { t.innerHTML = "Revisa los datos . . ."; setTimeout(function () { t.innerHTML = ". . ." }, 2000); return false; }
+  }
